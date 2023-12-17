eb9abfa26bade01b6f94462cb8816119c04e1bb5
Mayank-Saraswat <mayanksaraswat497@gmail.com>
2023-12-18 00:39:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,162 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import Square from "./Square";
 
 function App() {
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState("O");
+  const [result, setResult] = useState({ winner: "none", state: "none" });
+  const [resultMsg, setResultMsg] = useState('')
+  const [isBoardDisabled, setIsBoardDisabled] = useState(false);
+
+  const winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  useEffect(() => {
+    checkWin();
+    checkIfTie();
+
+    if (player === "X") {
+      setPlayer("O");
+    } else {
+      setPlayer("X");
+    }
+  }, [board]);
+
+  useEffect(() => {
+    if (result.state !== "none") {
+      setResultMsg(`Game Finished! Winning Player: ${result.winner}`);
+      setIsBoardDisabled(true);
+    }
+  }, [result]);
+
+  const chooseSquare = (square) => {
+    if (!isBoardDisabled) {
+      setBoard(
+        board.map((val, id) => {
+          if (id === square && val === "") {
+            return player;
+          }
+          return val;
+        })
+      );
+    }
+  };
+
+  const checkWin = () => {
+    winPatterns.forEach((currPattern) => {
+      const firstPlayer = board[currPattern[0]];
+      if (firstPlayer === "") return;
+      let foundWinningPattern = true;
+      currPattern.forEach((id) => {
+        if (board[id] !== firstPlayer) {
+          foundWinningPattern = false;
+        }
+      });
+
+      if (foundWinningPattern) {
+        setResult({ winner: player, state: "Won" });
+      }
+    });
+  };
+
+  const checkIfTie = () => {
+    let filled = true;
+    board.forEach((square) => {
+      if (square === "") {
+        filled = false;
+      }
+    });
+
+    if (filled) {
+      setResult({ winner: "No One", state: "Tie" });
+    }
+  };
+
+  const restartGame = () => {
+    setBoard(["", "", "", "", "", "", "", "", ""]);
+    setPlayer("O");
+    setResultMsg('')
+    setIsBoardDisabled(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {resultMsg !== '' ? <div className="resultMsg">{resultMsg}</div> : null}
+      <div className="App">
+        <div className="board">
+          <div className="row">
+            <Square
+              val={board[0]}
+              chooseSquare={() => {
+                chooseSquare(0);
+              }}
+            />
+            <Square
+              val={board[1]}
+              chooseSquare={() => {
+                chooseSquare(1);
+              }}
+            />
+            <Square
+              val={board[2]}
+              chooseSquare={() => {
+                chooseSquare(2);
+              }}
+            />
+          </div>
+          <div className="row">
+            <Square
+              val={board[3]}
+              chooseSquare={() => {
+                chooseSquare(3);
+              }}
+            />
+            <Square
+              val={board[4]}
+              chooseSquare={() => {
+                chooseSquare(4);
+              }}
+            />
+            <Square
+              val={board[5]}
+              chooseSquare={() => {
+                chooseSquare(5);
+              }}
+            />
+          </div>
+          <div className="row">
+            <Square
+              val={board[6]}
+              chooseSquare={() => {
+                chooseSquare(6);
+              }}
+            />
+            <Square
+              val={board[7]}
+              chooseSquare={() => {
+                chooseSquare(7);
+              }}
+            />
+            <Square
+              val={board[8]}
+              chooseSquare={() => {
+                chooseSquare(8);
+              }}
+            />
+          </div>
+        </div>
+      </div>
+      <button onClick={restartGame}>Reset</button>
+    </>
   );
 }
 
